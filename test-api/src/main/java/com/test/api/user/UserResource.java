591242c9522f573873db8ca535996edbd34882f6
Marcelo Toyoda <marcelo.toyoda@koin.com.br>
2019-03-09 03:16:15 -0300

    Configuracao de bean ObjectMapper

---
+++ test-api/src/main/java/com/test/api/user/UserResource.java
@@ -0,0 +1,4 @@
+package com.test.api.user;
+
+public class UserResource {
+}
---
+++ test-config/src/main/java/com/test/config/AppConfig.java
@@ -0,0 +1,23 @@
+package com.test.config;
+
+import org.modelmapper.Conditions;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setPropertyCondition(Conditions.isNotNull());
+        return modelMapper;
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
