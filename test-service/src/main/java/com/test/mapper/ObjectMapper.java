b32ff0111e8a03d0760b5b3d025fb66867cd99f6
Marcelo Toyoda <marcelo.toyoda@koin.com.br>
2019-03-09 03:16:49 -0300

    Inclusao de classe para mapper dto e entity

--- test-service/src/main/java/com/test/mapper/ObjectMapper.java
+++ test-service/src/main/java/com/test/mapper/ObjectMapper.java
@@ -1,4 +1,26 @@
 package com.test.mapper;
 
+import com.test.entity.User;
+import com.test.model.UserDTO;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Type;
+
+@Component
 public class ObjectMapper {
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public UserDTO convertToDto(User user) {
+        UserDTO userDTO = modelMapper.map(user, UserDTO.class);
+        return userDTO;
+    }
+
+    public User convertToEntity(UserDTO userDTO) {
+        User user = modelMapper.map(userDTO, User.class);
+        return user;
+    }
 }
