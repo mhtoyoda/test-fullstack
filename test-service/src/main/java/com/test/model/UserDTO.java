789d1ced44968ce61bb183c581c8b270d2afa07d
Marcelo Toyoda <marcelo.toyoda@koin.com.br>
2019-03-09 03:17:39 -0300

    Classe DTO para usuario entity

--- test-service/src/main/java/com/test/model/UserDTO.java
+++ test-service/src/main/java/com/test/model/UserDTO.java
@@ -1,4 +1,17 @@
 package com.test.model;
 
+import lombok.Data;
+
+@Data
 public class UserDTO {
+
+    private Long id;
+
+    private String nome;
+
+    private String email;
+
+    private String telefone;
+
+    private String sexo;
 }
