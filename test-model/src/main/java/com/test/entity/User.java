e6b2dc26197c0e7b38e62ece596a52647e202eb4
Marcelo Toyoda <marcelo.toyoda@koin.com.br>
2019-03-09 01:56:27 -0300

    Inclusao de entity e repository de Usuario

--- test-model/src/main/java/com/test/entity/User.java
+++ test-model/src/main/java/com/test/entity/User.java
@@ -1,4 +1,27 @@
 package com.test.entity;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "usuario")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "telefone")
+    private String telefone;
+
+    @Column(name = "sexo")
+    private String sexo;
 }
--- test-model/src/main/java/com/test/repository/UserRepository.java
+++ test-model/src/main/java/com/test/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.test.repository;
 
-public class UserRepository {
+import com.test.entity.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
 }
